30e3aa2abd2553274cbfcac08c5cf5060a625e2f
Faisal AlGhamdi <faisal.ghamdi92@hotmail.com>
2021-12-26 22:50:24 -0600

    fetched covid data & added Commons csv library

--- src/main/java/com/faisalghamdi/covid19tracker/services/CoronaVirusDataService.java
+++ src/main/java/com/faisalghamdi/covid19tracker/services/CoronaVirusDataService.java
@@ -1,10 +1,25 @@
 package com.faisalghamdi.covid19tracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.URI;
+import java.net.http.HttpResponse;
+@Service
 public class CoronaVirusDataService {
 
-    private static String COVID_DATA_URL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
-    public void fetchCovidData(){
+    private static String COVID_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    @PostConstruct
+    public void fetchCovidData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(COVID_DATA_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
 
     }
 
