8d7cd90109e53369b3f73da140d948ff14490c9a
Faisal AlGhamdi <faisal.ghamdi92@hotmail.com>
2021-12-26 23:35:15 -0600

    added scheduling to fetch data 1st hout of everyday

--- src/main/java/com/faisalghamdi/covid19tracker/services/CoronaVirusDataService.java
+++ src/main/java/com/faisalghamdi/covid19tracker/services/CoronaVirusDataService.java
@@ -1,9 +1,15 @@
 package com.faisalghamdi.covid19tracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.URI;
@@ -12,15 +18,24 @@ import java.net.http.HttpResponse;
 public class CoronaVirusDataService {
 
     private static String COVID_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
     @PostConstruct
+    // runs at first hour everyday
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchCovidData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(COVID_DATA_URL))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
 
+        StringReader csvBodyReader  = new StringReader(response.body());
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String provinceAndState = record.get("Province/State");
+            System.out.println(provinceAndState);
+            System.out.println(System.currentTimeMillis() / 1000);
+        }
     }
 
 }
